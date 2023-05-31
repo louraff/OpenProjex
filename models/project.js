0708e46d7336176d734b932cf1d797cebff6dd3b
Louise Raffray <raffraylouise@gmail.com>
2023-05-31 10:30:43 +0100

    added project schema

---
+++ models/project.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    name: String,
+    gitUsername: {type: Schema.Types.ObjectId, ref: 'User'},
+    projectURL: String,
+    description: String,
+    createdDate: {type: Date, default: Date.now},
+    createdBy: {type: Schema.Types.ObjectId, ref: 'User'},
+    comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
+    likes: {type: Schema.Types.ObjectId, ref: 'User'},
+    avatar: String
+  }, {
+    timestamps: true
+  });
+  
+  module.exports = mongoose.model('Project', projectSchema);
