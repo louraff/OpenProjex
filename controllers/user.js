e64aa6f33680d54e42d3ddbe4f6a49c4779cf6d5
Louise Raffray <raffraylouise@gmail.com>
2023-06-01 11:04:27 +0100

    added minimal styling to user profile page & a user can now add a new project from their profile page and it will display in the Your Projects tab

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Project = require('../models/project');
 
 module.exports = {
     show
@@ -6,11 +7,23 @@ module.exports = {
 
 async function show(req, res) {
     try {
-    const user = await User.findById(req.params.id)
-    res.render('user/show', {title: 'User Profile', user})
-    } catch (err) {
-    // Typically some sort of validation error
-    console.log(err);
-    res.render('index', { errorMsg: err.message });
+        const user = await User.findById(req.params.id);
+        const projects = await Project.find({ createdBy: user._id }).populate('createdBy');
+    
+        res.render('user/show', {title: 'User Profile', user, projects });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+      }
     }
-}
+    
+    
+//     try {
+//     const user = await User.findById(req.user._id)
+//     res.render('user/show', {title: 'User Profile', user})
+//     } catch (err) {
+//     // Typically some sort of validation error
+//     console.log(err);
+//     res.render('index', { errorMsg: err.message });
+//     }
+// }
